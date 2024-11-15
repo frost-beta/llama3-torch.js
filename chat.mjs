074c5aff8ac7d7d14642f17d977ca7d38cc87f81
Cheng <zcbenz@gmail.com>
2024-11-15 09:16:18 +0900

    Fix long prompt prefilling

--- chat.mjs
+++ chat.mjs
@@ -2,7 +2,7 @@
 
 import readline from 'node:readline/promises'
 import {styleText} from 'node:util'
-import {readFileSync} from 'node:fs'
+import {readFileSync, readdirSync} from 'node:fs'
 import {TokenizerLoader} from '@lenml/tokenizers'
 import {DType, Module, Tensor, sample} from 'executorch'
 
@@ -21,7 +21,8 @@ const tokenizer = TokenizerLoader.fromPreTrained({
 tokenizer.eosToken = tokenizer.model.tokens_to_ids.get(tokenizerConfig.eos_token)
 
 // Load model.
-const mod = new Module(`${dir}/llama3.2-instruct-executorch-kv-xnnpack-bf16.pte`)
+const modelFile = readdirSync(dir).find(f => f.endsWith('.pte'))
+const mod = new Module(`${dir}/${modelFile}`)
 await mod.load()
 
 // Enter chat loop.
@@ -69,29 +70,27 @@ async function talk(rl, tokenizer, mod, messages) {
 }
 
 async function* step(signal, tokenizer, mod, promptTokens) {
-  let token
+  let output
   let pos = promptTokens.length
   // Prefill prompt.
   const prefillStep = 512
-  for (let i = 0; i < prefillStep; i += prefillStep) {
+  for (let i = 0; i < promptTokens.length; i += prefillStep) {
     if (signal.aborted)
       return
     const end = Math.min(i + prefillStep, promptTokens.length)
-    const output = await mod.forward(new Tensor([ promptTokens.slice(i,  end) ], DType.Int64),
-                                     new Tensor([ i * prefillStep ], DType.Int64))
-    if (promptTokens.length < i + prefillStep)
-      token = sample(output, {topP: 0.9})
+    output = await mod.forward(new Tensor([ promptTokens.slice(i,  end) ], DType.Int64),
+                               new Tensor([ i ], DType.Int64))
   }
   // Forward.
   while (true) {
-    if (signal.aborted)
+    if (!output || signal.aborted)
       return
+    const token = sample(output, {topP: 0.9})
     if (token == tokenizer.eosToken)
       return
     yield token
-    const output = await mod.forward(new Tensor([ [ token ]], DType.Int64),
-                                     new Tensor([ pos ], DType.Int64))
-    token = sample(output, {topP: 0.9})
+    output = await mod.forward(new Tensor([ [ token ]], DType.Int64),
+                               new Tensor([ pos ], DType.Int64))
     pos++
   }
 }
